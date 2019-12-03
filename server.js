8d5a5a59f83bcf2e0211d366599fa40d9f919c25
GeekaN <geekan@bk.ru>
2019-12-03 23:15:34 +0300

    Heat distribution
    Add heat distributions graphic
    Add cool styles and theme

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/heat-ditribution-in-the-road', function (req, res) {
   res.sendFile(__dirname + '/static/heat-ditribution-in-the-road/index.html');
 });
 
+app.get('/', function (req, res) {
+  console.log(__dirname);
+  res.sendFile(__dirname + '/static/main/index.html');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
