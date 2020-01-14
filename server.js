605eae90aef97247643042544530cb9cd3c4fd9a
GeekaN <geekan@bk.ru>
2020-01-14 19:32:49 +0300

    Minor fixes

--- server.js
+++ server.js
@@ -4,20 +4,6 @@ var app = express();
 
 app.use(express.static('./static')); //This ensures local references to cs and js files work
 
-
-
-app.get('/anti-aircraft-gun', (req, res) => {
-  res.sendFile(__dirname + '/static/anti-aircraft-gun/index.html');
-});
-
-app.get('/heat-distribution-in-the-rod', function (req, res) {
-  res.sendFile(__dirname + '/static/heat-distribution-in-the-rod/index.html');
-});
-
-app.get('/heat-distribution-in-the-rod', function (req, res) {
-  res.sendFile(__dirname + '/static/rutherford-experiments/index.html');
-});
-
 app.get('/', function (req, res) {
   console.log(__dirname);
   res.sendFile(__dirname + '/static/main/index.html');
