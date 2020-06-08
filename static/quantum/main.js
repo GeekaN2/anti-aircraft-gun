ee0f5460745b3d4cdc686b3ee0163963c457a9c6
GeekaN <geekan@bk.ru>
2020-06-08 18:48:31 +0500

    Finish Ynl and Rnl charts

--- static/quantum/main.js
+++ static/quantum/main.js
@@ -1,11 +1,17 @@
 const z = 1; // порядковый номер атома
 const h = 1.05e-34; // приведённая постоянная планка
-const eq = 1.6e-19; // элементарный заряд
-const e0 = 8.854e-12; // электрическая постоянная
+const eq = 1.602176634e-19; // элементарный заряд
+const e0 = 8.854187817e-12; // электрическая постоянная
 const a = 0.529; // ангстрем
 const E0 = 27.07; // эВ
 const me = 9.1e-31; // масса электрона кг
 
+/**
+ * Факториал числа
+ * 
+ * @param {number} n - число
+ * @returns {number} - (1 * 2 * ... * n)
+ */ 
 function factorial(n) {
   let ans = 1;
   for (let i = n; i > 1; i--) {
@@ -15,14 +21,31 @@ function factorial(n) {
   return ans;
 }
 
+/**
+ * Косинус от угла в градусах
+ * 
+ * @param {number} angle - угол в градусах
+ * @returns {number} - косинус от угла
+ */ 
 function cos(angle) {
   return Math.cos(angle * Math.PI / 180);
 }
 
+/**
+ * Синус от угла в градусах
+ * 
+ * @param {number} angle - угол в градусах
+ * @returns {number} - косинус от угла
+ */ 
 function sin(angle) {
   return Math.sin(angle * Math.PI / 180);
 }
 
+/**
+ * Значение энергии электрона в водородоподобном атомне на уровне n
+ * 
+ * @param {number} n - 
+ */ 
 function E(n) {
   return (-(me * (eq ** 4) * (z ** 2)) / ((4 * Math.PI * e0) ** 2 * (2 * n ** 2 * h ** 2))) / eq;
 }
@@ -31,31 +54,33 @@ const eps = (n) => -E(n) / E0;
 const ro = (r) => r / a;
 const beta = (n) => Math.sqrt(2 * eps(n));
 
-
+// Нормировоный коэффициент для радиальной функции
 function Anl(n, l) {
   return (1 / factorial(2 * l + 1)) *
     Math.sqrt(factorial(n + l) / (2 * n * factorial(n - l - 1))) *
     (2 * z / n) ** 1.5;
 }
 
-// FIX PLEASE
+// Полиномы Лежандра для радиальной функции
 function Lnl(n, l, ro) {
-  // const ksi = 2 * beta(n) * ro;
-  const ksi = 2 * (z/n) * ro;
+  const ksi = 2 * beta(n) * ro;
   const gamma = n + l;
   const alpha = 2 * l + 1;
-  const Ly = nerdamer.diff(`exp(-ksi) * ksi^${gamma}`, 'ksi', gamma).evaluate().toString();
-  let Lya = nerdamer.diff(`exp(ksi) * ${Ly}`, 'ksi', alpha).evaluate({'ksi': ksi}).text();
-  return Lya;
+  const Lya = nerdamer.diff(nerdamer.diff(`exp(-ksi) * ksi^${gamma}`, 'ksi', gamma).multiply('exp(ksi)'), 'ksi', alpha).evaluate({ 'ksi': ksi }).text();
+
+  return Number(Lya);
 }
 
+// Считает значения радиальной функции состояния атома
 function Rnl(n, l, r) {
-  let ans = [ro(r).toFixed(1)];
+  let ans = {};
+  ans.x = ro(r).toFixed(1);
 
-  ans.push(Anl(n, l) *
+  ans.value = (Anl(n, l) *
     ro(r) ** l *
     Math.E ** (-beta(n) * ro(r)) *
     Lnl(n, l, ro(r)));
+
   return ans;
 }
 
@@ -81,88 +106,45 @@ function Ylm(l, m, teta) {
   return ans;
 }
 
-function mainY() {
+function Ylm_chart(l, m, chart) {
   let data = [];
-  chart = anychart.polar();
-  chart.xScale().maximum(360);
-  chart.container("container_Y");
 
-  const l = 3;
-  const m = 0;
+  // remove last series
+  chart.removeSeriesAt(0);
 
-  console.log(`l:${l} m:${m}`);
-
-  for (let angle = 0; angle < 360; angle += 1) {
+  for (let angle = 0; angle < 360; angle += 5) {
     data.push(Ylm(l, m, angle));
   }
-  console.log(data);
+  // console.log(data);
 
   let series = chart.line(data);
+  series.name(`Y${l}${m}(fi)`);
 
   chart.draw();
 }
 
-function mainR(n, l) {
-  // create data set on our data
-  var dataSet = anychart.data.set(getData(n, l));
+function Rnl_chart(data, title, chart) {
+  // delete last line
+  chart.removeSeriesAt(0);
 
-  // map data for the first series, take x from the zero column and value from the first column of data set
-  var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 });
-
-  // create line chart
-  var chart = anychart.line();
-
-  // turn on chart animation
-  chart.animation(true);
-
-  // set chart padding
-  chart.padding([10, 20, 5, 20]);
-
-  // turn on the crosshair
-  chart.crosshair().enabled(true).yLabel(false).yStroke(null);
-
-  // set tooltip mode to point
-  chart.tooltip().positionMode('point');
-
-  // set chart title text settings
-  /*chart.title(
-    'Trend of Sales of the Most Popular Products of ACME Corp.'
-  );*/
-
-  // set yAxis title
-  chart.yAxis().title(`R${n}${l}(ro)`);
-  chart.xAxis().labels().padding(5);
-
-  chart.xScale().mode('continuous');
-
-  // create first series with mapped data
-  var firstSeries = chart.line(firstSeriesData);
-  firstSeries.name(`R${n}${l}(ro)`);
-  firstSeries.hovered().markers().enabled(true).type('circle').size(4);
-  firstSeries
+  var series = chart.line(data);
+  series.name(title);
+  series.hovered().markers().enabled(true).type('circle').size(4);
+  series
     .tooltip()
     .position('right')
     .anchor('left-center')
     .offsetX(5)
     .offsetY(5);
 
-  // turn the legend on
-  chart.legend().enabled(true).fontSize(13).padding([0, 0, 10, 0]);
-
-  // set container id for the chart
-  chart.container('container_R');
-  // initiate chart drawing
   chart.draw();
 }
 
 function getData(n, l) {
   let data = [];
-  for (r = a; r < 10 * a; r += a/5) {
+  for (r = 0; r < 10 * a; r += a / 5) {
     data.push(Rnl(n, l, r));
   }
-  console.log(data);
+  // console.log(data);
   return data;
 }
-
-mainY();
-mainR(3, 1);
